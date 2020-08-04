d6c4971b0483345b361a4e1a569440c96fa42be3
wonmoLee <leewonmo94@gmail.com>
2020-08-04 17:34:53 +0900

    hooks 맛보기

---
+++ hooks-app/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useMemo } from "react";
+
+const getAverage = (numbers) => {
+  console.log("평균값 계산 중..");
+  if (numbers.length === 0) return 0;
+  const sum = numbers.reduce((a, b) => a + b); // a는 누적값
+  console.log(sum);
+  return sum / numbers.length; // 2
+};
+
+const App = () => {
+  const [list, setList] = useState([]);
+  const [number, setNumber] = useState("");
+  const memoAvg = useMemo(() => getAverage(list), [list]);
+
+  const onChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const onInsert = (e) => {
+    // [55]
+    const nextList = list.concat(parseInt(number));
+    setList(nextList);
+    setNumber("");
+  };
+
+  return (
+    <div>
+      <input value={number} onChange={onChange} />
+      <button onClick={onInsert}>등록</button>
+      <ul>
+        {list.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      <div>
+        <b>평균값:</b> {getAverage(list)}
+      </div>
+    </div>
+  );
+};
+
+export default App;
